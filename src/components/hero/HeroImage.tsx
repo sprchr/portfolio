9fee32e10255602dfa2c30f813c9faede598c690
sprchr <sprchrgreat@gmail.com>
2024-12-25 22:16:11 +0530

    Update HeroImage.tsx

--- src/components/hero/HeroImage.tsx
+++ src/components/hero/HeroImage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function HeroImage() {
   return (
     <img
-      src="https://i.ibb.co/Y72BLhv/Profile.jpg"
+      src="https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?w=800"
       alt="Development"
       className="rounded-2xl shadow-2xl"
     />
